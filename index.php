ce806bf369b465e149eb75f19577d69ec98e8661
Ludvig Alexander Bruchmann <info@ludvig.xyz>
2016-09-22 12:45:55 +0200

    Rounded bottom header

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html >
+<html>
 <head>
 
   <meta charset="UTF-8">
@@ -9,6 +9,10 @@
   <link rel='stylesheet prefetch' href='http://resources.ludvig.xyz/motus/css/motus.min.css'>
   <link rel='stylesheet prefetch' href='http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css'>
   <link rel="stylesheet" href="css/style.min.css">
+
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
 </head>
 
@@ -44,6 +48,7 @@
     </div>
   </div>
 </div>
+
 <div class="container">
   <div class="grid-12 center muted pt mb">
     Copyright &copy; Gefion Programming, <?php echo date("Y");?>
@@ -52,6 +57,7 @@
 
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
 <script src="js/index.js"></script>
+<script src="js/motus.js"></script>
 
 </body>
 </html>
