29e557a6dd56c79a57c1627ad6ca2055547abb4d
Ludvig Alexander Brüchmann <info@ludvig.xyz>
2016-10-06 12:12:04 +0200

    Global Variables
    head tag added as a global variable

---
+++ PHP/GlobalVar.php
@@ -0,0 +1,19 @@
+<?php
+  $GlobalHead = "
+  <meta charset='UTF-8'>
+  <title>Gefion Programming</title>
+
+  <link rel='stylesheet prefetch' href='http://resources.ludvig.xyz/normalize.css'>
+  <link rel='stylesheet prefetch' href='http://resources.ludvig.xyz/motus/css/motus.min.css'>
+  <link rel='stylesheet prefetch' href='http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css'>
+  <link rel='stylesheet' href='css/style.min.css'>
+
+  <link href='https://fonts.googleapis.com/css?family=Roboto:300,400' rel='stylesheet'>
+
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+  <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script src='js/motus.js'></script>
+  <script src='js/index.js'></script>
+  ";
+?>
