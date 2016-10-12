ff04a8c895b22c71de9416b8b5f4e5f2730f698d
JakobLui <jakob.l.abramson@gmail.com>
2016-10-12 15:43:34 +0200

    Bestemmer om den er åben eller lukket ved modulus 2

--- js/index.js
+++ js/index.js
@@ -1,35 +1,28 @@
+var s = 0
 $(document).click(function(){
-  console.log(event.target.id)
+  console.log(event.target.id);
   if (event.target.id != "navopen"){
     console.log("debug")
     $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
     $("#navclose").removeAttr("id").attr("id","navopen");
-  };
+    s += 1
+  } else if (s % 2 == 0){
+    $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
+    $("#navclose").removeAttr("id").attr("id","navopen");
+  }
 })
 function drop1(){
-    if (event.target.id == "navopen"){
-      $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
-      $(".dropdown1").removeClass("dropClosed").addClass("dropOpened");
-    }
-    else if (event.target.id == "navclose"){
-      $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
-    }
-  }
-  function drop2(){
-    if (event.target.id == "navopen"){
-      $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
-      $(".dropdown2").removeClass("dropClosed").addClass("dropOpened");
-    }
-    else if (event.target.id == "navclose"){
-      $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
-    }
-  }
-  function drop3(){
-    if (event.target.id == "navopen"){
-      $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
-      $(".dropdown3").removeClass("dropClosed").addClass("dropOpened");
-    }
-    else if (event.target.id == "navclose"){
-      $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
-    }
-  }
+  $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
+  $(".dropdown1").removeClass("dropClosed").addClass("dropOpened");
+  s += 1
+}
+function drop2(){
+  $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
+  $(".dropdown2").removeClass("dropClosed").addClass("dropOpened");
+  s += 1
+}
+function drop3(){
+  $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
+  $(".dropdown3").removeClass("dropClosed").addClass("dropOpened");
+  s += 1
+}
