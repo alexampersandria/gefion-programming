dd40b8dc1baf82333d94506a016870104a7343f7
Ludvig Alexander Bruchmann <info@ludvig.xyz>
2016-10-12 20:47:20 +0200

    About info and colorthingy margin

--- about.php
+++ about.php
@@ -44,9 +44,11 @@ session_start();
 
 <div class="container">
   <div class="grid-8 offset-2">
-    <h6 class="center">Tell-tale Heart, Edgar Allan Poe</h6>
     <p class="justify">
-      Presently I heard a slight groan, and I knew it was the groan of mortal terror. It was not a groan of pain or of grief --oh, no! --it was the low stifled sound that arises from the bottom of the soul when overcharged with awe. I knew the sound well. Many a night, just at midnight, when all the world slept, it has welled up from my own bosom, deepening, with its dreadful echo, the terrors that distracted me. I say I knew it well. I knew what the old man felt, and pitied him, although I chuckled at heart. I knew that he had been lying awake ever since the first slight noise, when he had turned in the bed. His fears had been ever since growing upon him. He had been trying to fancy them causeless, but could not. He had been saying to himself --"It is nothing but the wind in the chimney --it is only a mouse crossing the floor," or "It is merely a cricket which has made a single chirp." Yes, he had been trying to comfort himself with these suppositions: but he had found all in vain. All in vain; because Death, in approaching him had stalked with his black shadow before him, and enveloped the victim. And it was the mournful influence of the unperceived shadow that caused him to feel --although he neither saw nor heard --to feel the presence of my head within the room.
+      Gefion-Programming er en studiegruppe på Gefion Gymnasium, som har fokus på leg og udvikling med programmering og IT-værktøjer. Vi består af en gruppe af elever, alle med varierende evner og erfaring, som alle elsker at lege med diverse aspekter af computerverdenen. Vi laver alt fra webdesign til at lege med machine-learning algoritmer. Har du noget helt særligt i tankerne, som du gerne vil have hjælp til, eller lære at arbejde med, så er Gefion-Programming helt sikkert et sted for dig!
+    </p>
+    <p>
+      Målet med udvalget er for os at prøve kræfter med nye værktøjer og at tilbyde en platform for alle der er interesserede i programmering, hvor de kan diskuterere, lære, og finde andre med samme interesser.
     </p>
   </div>
 </div>
