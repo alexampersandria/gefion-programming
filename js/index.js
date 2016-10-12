8e119f4b26e8383701b2885db26b98740fefae89
JakobLui <jakob.l.abramson@gmail.com>
2016-10-12 14:27:23 +0200

    Dropdown

---
+++ js/index.js
@@ -0,0 +1,44 @@
+$(document).click(function(){
+  console.log(event.target.id)
+  if (event.target.id != "navopen" & event.target.id != "navclose"){
+    console.log("debug")
+    $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
+    $("#navclose").removeAttr("id").attr("id","navopen");
+  };
+})
+function drop1(){
+    if (event.target.id == "navopen"){
+      $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
+      $(".dropdown1").removeClass("dropClosed").addClass("dropOpened");
+      $("#navclose").removeAttr("id").attr("id","navopen");
+      $(event.target).removeAttr("id").attr("id","navclose");
+    }
+    else if (event.target.id == "navclose"){
+      $("#navclose").removeAttr("id").attr("id","navopen");
+      $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
+    }
+  }
+  function drop2(){
+    if (event.target.id == "navopen"){
+      $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
+      $(".dropdown2").removeClass("dropClosed").addClass("dropOpened");
+      $("#navclose").removeAttr("id").attr("id","navopen");
+      $(event.target).removeAttr("id").attr("id","navclose");
+    }
+    else if (event.target.id == "navclose"){
+      $(event.target).removeAttr("id").attr("id","navopen");
+      $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
+    }
+  }
+  function drop3(){
+    if (event.target.id == "navopen"){
+      $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
+      $(".dropdown3").removeClass("dropClosed").addClass("dropOpened");
+      $("#navclose").removeAttr("id").attr("id","navopen");
+      $(event.target).removeAttr("id").attr("id","navclose");
+    }
+    else if (event.target.id == "navclose"){
+      $(event.target).removeAttr("id").attr("id","navopen");
+      $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
+    }
+  }
