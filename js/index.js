2995683c2b107446777f85827a3085511c78acdc
JakobLui <jakob.l.abramson@gmail.com>
2016-10-12 14:36:29 +0200

    Hvorfor ændrede jeg id'en? xD

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).click(function(){
   console.log(event.target.id)
-  if (event.target.id != "navopen" & event.target.id != "navclose"){
+  if (event.target.id != "navopen"){
     console.log("debug")
     $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
     $("#navclose").removeAttr("id").attr("id","navopen");
@@ -10,11 +10,8 @@ function drop1(){
     if (event.target.id == "navopen"){
       $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
       $(".dropdown1").removeClass("dropClosed").addClass("dropOpened");
-      $("#navclose").removeAttr("id").attr("id","navopen");
-      $(event.target).removeAttr("id").attr("id","navclose");
     }
     else if (event.target.id == "navclose"){
-      $("#navclose").removeAttr("id").attr("id","navopen");
       $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
     }
   }
@@ -22,11 +19,8 @@ function drop1(){
     if (event.target.id == "navopen"){
       $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
       $(".dropdown2").removeClass("dropClosed").addClass("dropOpened");
-      $("#navclose").removeAttr("id").attr("id","navopen");
-      $(event.target).removeAttr("id").attr("id","navclose");
     }
     else if (event.target.id == "navclose"){
-      $(event.target).removeAttr("id").attr("id","navopen");
       $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
     }
   }
@@ -34,11 +28,8 @@ function drop1(){
     if (event.target.id == "navopen"){
       $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
       $(".dropdown3").removeClass("dropClosed").addClass("dropOpened");
-      $("#navclose").removeAttr("id").attr("id","navopen");
-      $(event.target).removeAttr("id").attr("id","navclose");
     }
     else if (event.target.id == "navclose"){
-      $(event.target).removeAttr("id").attr("id","navopen");
       $(".dropOpened").removeClass("dropOpened").addClass("dropClosed");
     }
   }
